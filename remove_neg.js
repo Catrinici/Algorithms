a0566cf44b1e71a439eb81e4a1b490d939561538
Catrinici <catrinici.antonina@gmail.com>
2019-10-11 20:04:29 -0700

    first commit

---
+++ remove_neg.js
@@ -0,0 +1,2 @@
+// Assignment: Removing Negatives
+// Given an array of multiple values (e.g. [0, -1, 2, -3, 4, -5, 6]), write a program that removes any negative values in that array.  Once your program is done, the array should be composed of only the non-negative numbers, in their original order.  Do this without creating a temporary array; only use the pop() method to remove values from the array.
